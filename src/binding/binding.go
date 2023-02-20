06f5fc3d41ee95ad668bc30ba623102739a8a95f
rambo <luwei.li@17zuoye.com>
2023-02-20 16:03:09 +0800

    feat: 使用反射进行Create功能创建

--- src/binding/binding.go
+++ src/binding/binding.go
@@ -6,9 +6,42 @@ package lvgl_go
 #cgo LDFLAGS: -Llib -llvgl
 */
 import "C"
-import "unsafe"
+import (
+	"reflect"
+	"strings"
+	"unsafe"
+)
 
 type LvObj C.struct__lv_obj_t
+
+type lv struct {
+	t string
+}
+
+func (f lv) ToMethoName() string {
+	return strings.Join([]string{"Create", strings.ToUpper(f.t[:1]), f.t[1:]}, "")
+}
+func (f lv) CreateLabel(parent *C.struct__lv_obj_t) *C.struct__lv_obj_t {
+	return C.lv_label_create(parent)
+}
+func (f lv) CreateScreen(_ *C.struct__lv_obj_t) *C.struct__lv_obj_t {
+	return C.lv_obj_create(nil)
+}
+
+func Create(t string, parent *LvObj) *LvObj {
+	if parent == nil {
+		parent = (*LvObj)(unsafe.Pointer(C.lv_scr_act()))
+	}
+
+	_lv := &lv{t}
+
+	in := make([]reflect.Value, 1)
+	in[0] = reflect.ValueOf((*C.struct__lv_obj_t)(unsafe.Pointer(parent)))
+
+	ret := reflect.ValueOf(_lv).MethodByName(_lv.ToMethoName()).Call(in)
+
+	return (*LvObj)(ret[0].Interface().(unsafe.Pointer))
+}
 
 func Label(parent *LvObj) *LvObj {
 	if parent == nil {
