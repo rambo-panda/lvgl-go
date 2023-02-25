d88538c428c8edfb2438809e76dae148aff8f76b
rambo <luwei.li@17zuoye.com>
2023-02-25 12:00:43 +0800

    feat: 使用反射安全操作。 目前还不太清楚为什么强转换会报错

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	lvgl_go "lvgl-go/src"
+	"lvgl-go/src/create"
 	"lvgl-go/src/lib"
 )
 
 func main() {
 	lib.Ready()
-	label := lvgl_go.CreateLabel(nil)
-	label.Set.SetText("jjjjj")
+	label := create.CreateLabel(nil)
+	label.Set.Text("jjjjj")
 	// lvgl_go.Demo()
 	lib.TaskHandler(0)
 }
