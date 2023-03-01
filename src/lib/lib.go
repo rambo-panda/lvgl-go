d90d8304f58243cfa72d5ddc4ef697260361e3fd
rambo <luwei.li@17zuoye.com>
2023-03-01 11:13:53 +0800

    feat: 暂时去掉isNil

--- src/lib/lib.go
+++ src/lib/lib.go
@@ -7,7 +7,6 @@ package lib
 */
 import "C"
 import (
-	"reflect"
 	"unsafe"
 )
 
@@ -36,16 +35,6 @@ func Go2CString(o string) unsafe.Pointer {
 	//TODO: 何时删除?? lvgl应该会自己删除 C.free(unsafe.Pointer(cs))
 
 	return unsafe.Pointer(cs)
-}
-
-func IsNil(o any) bool {
-	if o == nil {
-		return true
-	}
-
-	l := reflect.ValueOf(o)
-
-	return l.Kind() == reflect.Pointer && l.IsNil()
 }
 
 func Ternary[T any](c bool, trueVal, falseVal T) T {
